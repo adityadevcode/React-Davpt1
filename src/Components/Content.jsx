d48992f0b44da5a494232154d0cc0f51fe97fe74
addycoderepo <adityagitcode@gmail.com>
2022-09-15 23:41:53 -0400

    React Lists and Keys/ saving state to local storage/onClick onDouble click/delete fucntion

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -83,60 +83,147 @@
 //////React List and keys
 
 
-import {useState} from 'react';
-//Fontawesome TrashAlt Icon 
-import {FaTrashAlt} from 'react-icons/fa';
+// import {useState} from 'react';
+// //Fontawesome TrashAlt Icon 
+// import {FaTrashAlt} from 'react-icons/fa';
+
+// const Content = () => {
+//     // default state can be string, array
+//     const [items, setItems] = useState([
+//         // we have array contain3 objects, each obj has id,checked and item property
+//     {
+//         id:1, //string
+//         checked:false, //boolean
+//         // checked:true,
+//         item:"one pound"
+//     },
+//     {
+//         id:2,
+//         checked:false,
+//         // checked:true,
+//         item:"one kilo" 
+//     },
+//     {
+//         id:3,
+//         checked:false,
+//         // checked:true,
+//         item:"one tonn" 
+//     }
+//     ]);
+
+//     return(
+//         <main>
+// <ul>
+//     {/* map is higher order fucntion */}
+//     {/* each list in react need key */}
+//     {/* by using id react know which items are added/deleted and updated */}
+//     {items.map((item) => (
+//         <li className="item" key={item.id}>
+//              <input 
+//              type="checkbox"
+//              checked={item.checked}
+//              />
+//              <label>{item.item}</label>
+//              {/* <button>Delete</button> */}
+//              <FaTrashAlt 
+//              role="button" 
+//              tabIndex="0"
+//              />
+//         </li>
+//     ))}
+// </ul>
+//         </main>
+//     )
+// }
+
+// export default Content;
+
+//why dont we see when checkmark when we click because
+//boolean is set to false if it is true we can see. this will come from state
+//  checked={item.checked}
+
+//Adding onChange Listner , handleCheck function
+//with check box we dont need onClick event we need onChange event, we need to pass anaony,ous func
+//we dont want to change state directly we use map(hoc) 
+//setItems to change the state
+//it will not change default state of the app 
+//view new component state in react dev tools
+//when we reload the app it will come back as default state not the change we made for this purpose 
+/*  we need to save the state and bring backs when app loads
+we use local storage, we set an item and use json.stringfy it will save undershopping list in local storage   
+-saving state lo local storage
+-adding an ondoubleclick listner
+-add conditional css styling
+-adding on onClick listner
+-handle delete function 
+-adding empty list message
+-when we will load state from local storage
+*/
+
+//final code
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
-    // default state can be string, array
     const [items, setItems] = useState([
-        // we have array contain3 objects, each obj has id,checked and item property
-    {
-        id:1, //string
-        checked:false, //boolean
-        item:"one pound"
-    },
-    {
-        id:2,
-        checked:false,
-        item:"one kilo" 
-    },
-    {
-        id:3,
-        checked:false,
-        item:"one tonn" 
-    }
+        {
+            id: 1,
+            checked: true,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
     ]);
 
-    return(
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    return (
         <main>
-<ul>
-    {/* map is higher order fucntion */}
-    {/* each list in react need key */}
-    {/* by using id react know which items are added/deleted and updated */}
-    {items.map((item) => (
-        <li className="item" key={item.id}>
-             <input 
-             type="checkbox"
-             checked={item.checked}
-             />
-             <label>{item.item}</label>
-             {/* <button>Delete</button> */}
-             <FaTrashAlt 
-             role="button" 
-             tabIndex="0"
-             />
-        </li>
-    ))}
-</ul>
+            {/* ternary operator incase if it is not display anything */}
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className="item" key={item.id}>
+                            <input
+                                type="checkbox"
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                            <label
+                                style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                                onDoubleClick={() => handleCheck(item.id)}
+                            >{item.item}</label>
+                            <FaTrashAlt
+                                onClick={() => handleDelete(item.id)}
+                                role="button"
+                                tabIndex="0"
+                            />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+            )}
         </main>
     )
 }
 
-export default Content;
-
-
-
-
-
-
+export default Content
